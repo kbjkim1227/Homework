9321ff01049a727d11e748b31f6b3d7a355e5e91
Bongjae <kbjkim@naver.com>
2022-05-25 17:54:22 +0900

    20220525업로드

---
+++ homework/src/homework1_13/Human.java
@@ -0,0 +1,21 @@
+package homework1_13;
+// 사람 클래스(ver.1)
+class Human {
+	String name;	//이름
+	int height;		//신장
+	int weight;		//체중
+	
+	// --- 생성자 ---//
+	Human(String n, int h, int w){
+		name = n; height = h; weight = w;
+	}
+	
+	String getName() { return name; }
+	int getHeight() { return height; }
+	int getWeight() { return weight; }
+	
+	void gainWeight(int w) {weight += w;}
+	void reduceWeight(int w) {weight -= w;}
+	
+	
+}
---
+++ homework/src/homework1_13/HumanTester.java
@@ -0,0 +1,22 @@
+package homework1_13;
+//사람 클래스(ver.1) 사용예
+public class HumanTester {
+
+	public static void main(String[] args) {
+		Human gildong = new Human("길동", 170, 60);
+		Human chulsu = new Human("철수", 166, 72);
+		
+		gildong.gainWeight(3);
+		chulsu.reduceWeight(5);
+		
+		System.out.println("이름 :" + gildong.name);
+		System.out.println("이름 :" + gildong.height + "cm");
+		System.out.println("이름 :" + gildong.weight + "kg");
+		System.out.println();
+		
+		System.out.println("이름 :" + chulsu.name);
+		System.out.println("이름 :" + chulsu.height + "cm");
+		System.out.println("이름 :" + chulsu.weight + "kg");
+	}
+
+}
